5811e41d8328fa919629a19c8cba10e9c412992c
Devon Fulcher <devonfulcher3@gmail.com>
2024-01-18 22:44:06 -0600

    fixed safe pull

--- scripts/git_safe_pull.sh
+++ scripts/git_safe_pull.sh
@@ -11,8 +11,19 @@ fi
 # Fetch the latest changes
 git fetch
 
+if git rev-parse --verify main >/dev/null 2>&1; then
+    # If 'main' exists, checkout to 'main'
+    default_branch="main"
+elif git rev-parse --verify master >/dev/null 2>&1; then
+    # If 'master' exists but 'main' does not, checkout to 'master'
+    default_branch="master"
+else
+    echo "Neither 'main' nor 'master' branch exists."
+    exit 1
+fi
+
 # Check for potential merge conflicts
-if ! git merge-base --is-ancestor HEAD origin/master; then
+if ! git merge-base --is-ancestor HEAD origin/$default_branch; then
     echo "Potential merge conflicts detected in $PWD. Skipping."
     exit 0
 fi
