c552522a3e20b014ff4cf4d475c1f798cac17f18
Devon Fulcher <devonfulcher3@gmail.com>
2024-01-27 17:28:10 -0600

    added reviewDecision to git_pr_list

--- scripts/git_pr_list.sh
+++ scripts/git_pr_list.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-
 # Fetch PRs and filter them based on author or review requests
-json_output=$(gh pr list --json number,title,author,reviewRequests --jq 'map(select(.author.login == "DevonFulcher" or any(.reviewRequests[]; .login == "DevonFulcher")))')
+json_output=$(gh pr list --json number,title,author,reviewDecision,reviewRequests --jq 'map(select(.author.login == "DevonFulcher" or any(.reviewRequests[]; .login == "DevonFulcher")))')
 
 # Check if there is any output to avoid errors with jq
 if [ -z "$json_output" ]; then
@@ -9,7 +7,7 @@ if [ -z "$json_output" ]; then
   exit 0
 fi
 
-header="Number\tTitle\tAuthor\tReviewers\n"
+header="Number\tTitle\tAuthor\tReview Decision\tReviewers\n"
 
 # Format the JSON output into a table without the -n option
-echo "$json_output" | jq -r '.[] | [.number, .title, .author.login, ([.reviewRequests[].login] | join(", "))] | @tsv' | sed "1s/^/$header/" | column -t -s $'\t'
+echo "$json_output" | jq -r '.[] | [.number, .title, .author.login, .reviewDecision, ([.reviewRequests[].login] | join(", "))] | @tsv' | sed "1s/^/$header/" | column -t -s $'\t'
