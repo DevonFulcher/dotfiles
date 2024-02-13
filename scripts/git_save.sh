6853eeee8959ab8b54b02887f333cd412a7657a0
Devon Fulcher <devonfulcher3@gmail.com>
2024-02-12 20:33:48 -0600

    undo --no-verify

--- scripts/git_save.sh
+++ scripts/git_save.sh
@@ -1,17 +1,6 @@
 git add -A;
 if [ -n "$1" ]; then
-  verify=true;
-  echo "$@"
-  for arg in "$@"; do
-    if [ "$arg" = "--no-verify" ]; then
-      verify=false;
-    fi
-  done
-  if [ $verify ]; then
-    git commit -m "$1" --quiet && echo "code committed" || { echo "commit failed"; exit 1; }
-  else
-    git commit -m "$1" --quiet --no-verify && echo "code committed" || { echo "commit failed"; exit 1; }
-  fi
+  git commit -m "$1" --quiet && echo "code committed" || { echo "commit failed"; exit 1; }
 else
   aicommits || { echo "aicommits failed"; exit 1; }
 fi
