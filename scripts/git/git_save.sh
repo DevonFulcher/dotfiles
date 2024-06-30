e2431fecd7ce0b80a8c732523b69efebd92f80ce
Devon Fulcher <24593113+DevonFulcher@users.noreply.github.com>
2024-06-29 22:59:17 -0500

    "updated gitconfig"

--- scripts/git/git_save.sh
+++
@@ -1,7 +0,0 @@
-git add -A;
-if [ -n "$1" ]; then
-  git commit -m "$1" --quiet && echo "code committed" || { echo "commit failed"; exit 1; }
-else
-  aicommits || { echo "aicommits failed"; exit 1; }
-fi
-git push --quiet && echo "commit pushed" || echo "push failed";
